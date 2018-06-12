5f58adbf5dd6c706393e6b0f9359aad49f80c8d3
Pablo Brudnick <pbrudnick@gmail.com>
2018-06-12 16:58:02 -0300

    preparing mix.exs for Hex publish

--- mix.exs
+++ mix.exs
@@ -1,13 +1,18 @@
 defmodule BtrzWebhooksDeniedFields.MixProject do
   use Mix.Project
 
+  @github_url "https://github.com/Betterez/btrz_webhooks_denied_fields"
+  @version "0.1.0"
+
   def project do
     [
       app: :btrz_webhooks_denied_fields,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      docs: docs(),
+      package: package(),
       aliases: aliases(),
       test_coverage: [tool: ExCoveralls]
     ]
@@ -32,5 +37,23 @@ defmodule BtrzWebhooksDeniedFields.MixProject do
       {:poison, "~> 3.0"},
       {:excoveralls, "~> 0.8", only: :test}
     ]
+  end
+
+  defp docs do
+    [
+      main: "BtrzWebhooksDeniedFields",
+      source_ref: "v#{@version}",
+      source_url: @github_url,
+      extras: ["README.md"]
+    ]
+  end
+
+  defp package do
+    %{
+      name: "btrz_webhooks_denied_fields",
+      licenses: ["MIT"],
+      maintainers: ["Hernán García", "Pablo Brudnick"],
+      links: %{"GitHub" => @github_url}
+    }
   end
 end
